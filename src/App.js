c4abacf5a1de29fa90178a3ac8c56d4276c173e2
VoldemarTamm <tamm.voldemar@gmail.com>
2024-02-18 11:57:38 +0200

    navbar sai reaalselt valmis. Pages on ka ühendatud.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
-import { Navbar } from "./Navbar";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Recipe } from "./pages/Recipes";
+import { Shoppinglist } from "./pages/Shoppinglist";
+import { Wishlist } from "./pages/Wishlist";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe" element={<Recipe />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/shoppinglist" element={<Shoppinglist />} />
+        </Routes>
+      </div>
     </div>
   );
 }
