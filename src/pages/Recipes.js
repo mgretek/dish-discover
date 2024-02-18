68eba9df265ff338fb8faae06cb6fd5a0d0b143d
Fredi Karu <fredikaru@gmail.com>
2024-02-19 01:15:15 +0200

    Created search filter and type buttons

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -3,6 +3,16 @@ import { useState } from "react";
 export const Recipe = () => {
   const [searchType, setSearchType] = useState("name");
   const [filter, setFilter] = useState("both");
+  const [searchInput, setSearchInput] = useState("");
+
+  function handleFilter(input) {
+    setFilter(input);
+  }
+
+  function handleInput(input) {
+    setSearchInput(input);
+  }
+  function handleSearch() {}
 
   return (
     <div className="lg:px-32">
@@ -12,23 +22,56 @@ export const Recipe = () => {
           <input
             placeholder="Search..."
             className="bg-gray-50 flex border border-gray-300 text-gray-900 text-sm w-full h-10"
+            onChange={(e) => handleInput(e.target.value)}
           ></input>
-          <button className="bg-gray-400 rounded-sm px-5">Search</button>
+          <button
+            className="bg-gray-400 rounded-sm px-5"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
         <div class="flex">
           <div className="flex">
             <p className="pr-3">Filter:</p>
-            <button class="btn">Meals</button>
+            <button
+              className={`btn ${filter === "meals" ? "underline" : ""}`}
+              onClick={() => handleFilter("meals")}
+            >
+              Meals
+            </button>
             <span className="lg:px-2">/</span>
-            <button class="btn">Drinks</button>
+            <button
+              className={`btn ${filter === "drinks" ? "underline" : ""}`}
+              onClick={() => handleFilter("drinks")}
+            >
+              Drinks
+            </button>
             <span className="lg:px-2">/</span>
-            <button class="btn">Both</button>
+            <button
+              className={`btn ${filter === "both" ? "underline" : ""}`}
+              onClick={() => handleFilter("both")}
+            >
+              Both
+            </button>
           </div>
           <div className="flex ml-auto">
             <p className="pr-3">Search by:</p>{" "}
-            <button class="btn">Incredients</button>
+            <button
+              className={`btn ${
+                searchType === "incredients" ? "underline" : ""
+              }`}
+              onClick={() => setSearchType("incredients")}
+            >
+              Incredients
+            </button>
             <span className="lg:px-2">/</span>
-            <button class="btn">Name</button>
+            <button
+              className={`btn ${searchType === "name" ? "underline" : ""}`}
+              onClick={() => setSearchType("name")}
+            >
+              Name
+            </button>
           </div>
         </div>
       </div>
@@ -38,46 +81,19 @@ export const Recipe = () => {
             <div className="flex">
               <div className="bg-gray-500 w-32 h-36"></div>
               <div className="flex flex-col px-8">
-                <h2 className="text-4xl">Pancakes</h2>
+                <h2 className="text-4xl text-left">Pancakes</h2>
                 <ul className="flex gap-2">
                   <li>Egg</li>
                   <li>Flour</li>
                   <li>sugar</li>
                 </ul>
+                <div class="flex text-gray-400 gap-x-2">
+                  <p>#dinner</p>
+                  <p>#breakfast</p>
+                  <p>#breakfast</p>
+                </div>
                 <p className="text-sm bg-gray-500 text-white rounded mt-auto">
                   World
-                </p>
-              </div>
-            </div>
-          </li>
-          <li className="bg-white p-4 rounded mb-4">
-            <div className="flex">
-              <div className="bg-gray-500 w-32 h-36"></div>
-              <div className="flex flex-col px-8">
-                <h2 className="text-4xl">Avocado</h2>
-                <ul className="flex gap-2">
-                  <li>Egg</li>
-                  <li>Flour</li>
-                  <li>sugar</li>
-                </ul>
-                <p className="text-sm bg-gray-500 text-white rounded mt-auto">
-                  Estonian
-                </p>
-              </div>
-            </div>
-          </li>
-          <li className="bg-white p-4 rounded mb-4">
-            <div className="flex">
-              <div className="bg-gray-500 w-32 h-36"></div>
-              <div className="flex flex-col px-8">
-                <h2 className="text-4xl">Pancakes</h2>
-                <ul className="flex gap-2">
-                  <li>Egg</li>
-                  <li>Flour</li>
-                  <li>sugar</li>
-                </ul>
-                <p className="text-sm bg-gray-500 text-white rounded mt-auto">
-                  Italian
                 </p>
               </div>
             </div>
