4ffc895bef7e60e31d9637fb3a8daf74e3cd33aa
Fredi Karu <fredikaru@gmail.com>
2024-02-22 01:58:38 +0200

    Replaced buttons with icons

---
+++ src/components/DownArrow.js
@@ -0,0 +1,21 @@
+export const DownArrow = ({ onClick }) => {
+  return (
+    <svg
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      className="ionicon"
+      viewBox="0 0 512 512"
+      width={24}
+      height={24}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={48}
+        d="M112 184l144 144 144-144"
+      />
+    </svg>
+  );
+};
--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { DownArrow } from "./DownArrow";
+import { ForwardArrow } from "./ForwardArrow";
 
 export const Wishlist = ({ title, children }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -7,10 +9,17 @@ export const Wishlist = ({ title, children }) => {
     setIsCollapsed(!isCollapsed);
   }
   return (
-    <div className="text-left">
-      <h1 className="text-3xl underline">{title}</h1>
-      <button onClick={toggleCollapse}>Collapse</button>
-      {!isCollapsed && <div>{children}</div>}
+    <div className="text-left mb-3">
+      <div class="flex bg-white justify-between px-4 py-3">
+        <h1 className="text-3xl bg-white flex-2">{title}</h1>
+
+        {isCollapsed ? (
+          <ForwardArrow onClick={toggleCollapse} />
+        ) : (
+          <DownArrow onClick={toggleCollapse} />
+        )}
+      </div>
+      {!isCollapsed && <div className="bg-orange-200 p-3">{children}</div>}
     </div>
   );
 };
