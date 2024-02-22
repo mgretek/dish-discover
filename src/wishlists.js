1ca336ed998005aa412c2215ad0c3cad431adb9c
Fredi Karu <fredikaru@gmail.com>
2024-02-23 01:29:52 +0200

    Added a firebase import to wishlists.js

--- src/wishlists.js
+++ src/wishlists.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
 export async function createWishlist() {}
 
 export async function addToWishlist(listId, recipe) {}
