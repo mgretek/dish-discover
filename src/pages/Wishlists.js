7a71c1a435fe839cbe3c5f1299891ca7684df713
Fredi Karu <fredikaru@gmail.com>
2024-02-22 02:33:01 +0200

    Added search field to wishlists page

--- src/pages/Wishlists.js
+++ src/pages/Wishlists.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { Wishlist, WishItem } from "../components/Wishlist";
 
 export const Wishlists = () => {
+  const [filter, setFilter] = useState("drinks");
+
   return (
     <div className="bg-orange-100 md:px-20 lg:px-60">
-      <h2 className="text-5xl text-left">Your wishlists</h2>
+      <div class="flex py-10 justify-between">
+        <h2 className="text-5xl text-left ">Your wishlists</h2>
+        <input
+          placeholder="Search recipes..."
+          className="bg-gray-50 flex border border-gray-300 text-gray-900 text-sm  h-auto"
+        ></input>
+      </div>
       <Wishlist title={"Dinner"}>
         <WishItem />
         <WishItem />
