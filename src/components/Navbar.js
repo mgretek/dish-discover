e31b0cec76234ea0d68c7784cd5590508fa91dc9
Merilin-Grete Kakko <merilingrete.kakko@gmail.com>
2024-03-04 22:38:53 +0200

    Minor improvements on navbar. Improvements on the desktop view on single recipe.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,27 @@ export const Navbar = () => {
     <nav className="bg-gradient-to-r from-neutral-50 via-pink-200 to-violet-200 p-4 flex flex-wrap md:flex-nowrap justify-between gap-y-3 md:gap-6">
       <Link
         to="/"
-        className="text-3xl font-extrabold bg-gradient-to-r from-rose-400 to-violet-500 bg-clip-text text-transparent">
+        className="text-3xl font-extrabold bg-gradient-to-r from-rose-400 to-violet-500 bg-clip-text text-transparent"
+      >
         Wish Dish Discover
       </Link>
       <ul className="flex gap-2 md:gap-6 items-center">
-        <CustomLink to={"/recipes"} className="text-md text-gray-700">
+        <CustomLink
+          to={"/recipes"}
+          className="text-md text-gray-700 hover:text-violet-500"
+        >
           Recipes
         </CustomLink>
-        <CustomLink to={"/wishlists"} className="text-md text-gray-700">
+        <CustomLink
+          to={"/wishlists"}
+          className="text-md text-gray-700 hover:text-violet-500"
+        >
           Wishlist
         </CustomLink>
-        <CustomLink to={"/shoppinglist"} className="text-md text-gray-700">
+        <CustomLink
+          to={"/shoppinglist"}
+          className="text-md text-gray-700 hover:text-violet-500"
+        >
           Shopping List
         </CustomLink>
         <Link>
