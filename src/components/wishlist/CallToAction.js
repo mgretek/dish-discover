dcda7c3623e284d990333eedbe386fa46e876922
Fredi Karu <fredikaru@gmail.com>
2024-03-05 23:12:51 +0200

    Make call to action into component

---
+++ src/components/wishlist/CallToAction.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CallToAction() {
+  return (
+    <div>
+      <h2 className="text-5xl text-left ">Please log in to use whislists</h2>
+      <p className="text-left text-xl mt-7 ">
+        Over here you can create lists of your favourite recipes. Imagine
+        finding those exact pancakes that would make an excellent sunday morning
+        dish. Create a special Sunday list and add the recipe to your list, so
+        you would find those pancakes easily with just a couple of clicks!
+      </p>
+    </div>
+  );
+}
