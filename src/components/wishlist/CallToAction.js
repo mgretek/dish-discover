cea04cca49fd6b8b931f1c458aacb36639f584b0
Merilin-Grete Kakko <merilingrete.kakko@gmail.com>
2024-03-07 00:00:44 +0200

    Styling of Wishlist and Shopping List pages if not logged in

--- src/components/wishlist/CallToAction.js
+++ src/components/wishlist/CallToAction.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function CallToAction() {
   return (
-    <div>
-      <h2 className="text-5xl text-left ">Please log in to use whislists</h2>
-      <p className="text-left text-xl mt-7 ">
-        Over here you can create lists of your favourite recipes. Imagine
-        finding those exact pancakes that would make an excellent sunday morning
-        dish. Create a special Sunday list and add the recipe to your list, so
-        you would find those pancakes easily with just a couple of clicks!
+    <div className="px-6 sm:px-16 mt-8 sm:mt-16 mx-6 sm:mx-auto max-w-[620px] min-h-full border-2 border-violet-200 rounded-md">
+      <h2 className="mt-14 text-center text-3xl sm:text-5xl font-semibold text-gray-800 mb-10">
+        Please log in to use wishlists
+      </h2>
+      <p className="text-md sm:text-lg text-center mb-16 text-gray-700">
+        Here you can create lists of your favourite recipes. Imagine finding
+        those exact pancakes that would make an excellent sunday morning dish.
+        Create a special Sunday list and add the recipe to your list, so you
+        would find those pancakes easily with just a couple of clicks!
       </p>
     </div>
   );
--- src/pages/Shoppinglist.js
+++ src/pages/Shoppinglist.js
@@ -5,19 +5,19 @@ export const Shoppinglist = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <div className="bg-orange-100 md:px-20 xl:px-60 min-h-full">
+    <div className="px-6 sm:px-16 mt-8 sm:mt-16 mx-6 sm:mx-auto max-w-[620px] min-h-full border-2 border-violet-200 rounded-md">
       {user ? (
         <h1>Shopping list</h1>
       ) : (
         <div>
-          <h1 className="text-5xl text-left ">
-            Please log in to use the shopping list!
+          <h1 className="mt-14 text-center text-3xl sm:text-5xl font-semibold text-gray-800 mb-10">
+            Please log in to use the shopping list
           </h1>
-          <p className="mt-6">
-            Over here you can add chosen ingriedients from your found recipes,
-            to make yourself a shopping list, to make your life just a little
-            bit easier. No more manually copying stuff you need to buy.
-            Everything is just a mouse click away!{" "}
+          <p className="text-md sm:text-lg text-center mb-16 text-gray-700">
+            Here you can add chosen ingredients from your found recipes, to make
+            yourself a shopping list, to make your life just a little bit
+            easier. No more manually copying stuff you need to buy. Everything
+            is just a mouse click away!{" "}
           </p>
         </div>
       )}
