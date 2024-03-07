56b0e33b20758058dcb060f856d8013497d63877
Merilin-Grete Kakko <merilingrete.kakko@gmail.com>
2024-03-07 23:43:47 +0200

    Fixed footer responsivity issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,29 +4,37 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <div className="mt-8 pt-6 pb-6 w-full bg-gradient-to-b from-violet-200 to-pink-200 overflow-hidden">
-      <div className="md:mx-3 text-gray-700 xl:max-w-6xl xl:mx-auto max-w-[576px] md:max-w-full mx-auto px-3">
-        <div className="font-extrabold text-2xl mb-4">Wish Dish Discover</div>
-        <div className="flex md:gap-x-2.5 lg:gap-x-10 mb-2 flex-wrap ">
+      <div className="text-gray-700 mx-auto px-4 xl:pl-20 md:pl-10">
+        <div className="font-extrabold text-2xl mb-2">Wish Dish Discover</div>
+        <div className="flex md:gap-x-2.5 lg:gap-x-10 mb-2 flex-wrap md:flex-nowrap justify-between">
           <div>
             <div className="mt-6 max-w-full md:max-w-[80%] mb-8">
               Discover thousands of meals, drinks & cocktails, add them to
               wishlists and generate automatic shopping lists.
             </div>
           </div>
-          <div className="flex gap-x-16 lg:gap-x-36 xl:gap-x-48 2xl:gap-x-64 mb-4">
+          <div className="flex gap-x-16 mt-2 lg:gap-x-36 xl:gap-x-48 mb-4 xl:pr-32 md:pr-20 2xl:pr-48">
             <div className="flex flex-col">
               <div className="truncate font-extrabold mb-2">Discover more</div>
-              <Link to="/wishlists" className="text-sm mb-2">
+              <Link
+                to="/wishlists"
+                className="text-sm mb-2 hover:text-gray-800"
+              >
                 Wishlists
               </Link>
-              <Link to="/shoppinglist" className="text-sm mb-2">
+              <Link
+                to="/shoppinglist"
+                className="text-sm mb-2 hover:text-gray-800"
+              >
                 Shopping list
               </Link>
             </div>
             <div>
               <div className="font-extrabold mb-2">Useful</div>
-              <div className="truncate text-sm mb-2">Terms & Conditions</div>
-              <div className="truncate text-sm mb-2">
+              <div className="truncate text-sm mb-2 hover:text-gray-800">
+                Terms & Conditions
+              </div>
+              <div className="truncate text-sm mb-2 hover:text-gray-800">
                 Links to GitHub orsmth
               </div>
             </div>
@@ -39,3 +47,43 @@ export const Footer = () => {
     </div>
   );
 };
+
+{
+  /* initial working code:
+<div className="mt-8 pt-6 pb-6 w-full bg-gradient-to-b from-violet-200 to-pink-200 overflow-hidden">
+  <div className="md:mx-3 text-gray-700 xl:max-w-6xl xl:mx-auto max-w-[576px] md:max-w-full mx-auto px-3">
+    <div className="font-extrabold text-2xl mb-4">Wish Dish Discover</div>
+    <div className="flex md:gap-x-2.5 lg:gap-x-10 mb-2 flex-wrap md:flex-nowrap">
+      <div className="max-w-[60%]">
+        <div className="mt-6 max-w-full md:max-w-[50%] mb-8">
+          Discover thousands of meals, drinks & cocktails, add them to wishlists
+          and generate automatic shopping lists.
+        </div>
+      </div>
+      <div className="flex gap-x-16 lg:gap-x-36 xl:gap-x-48 2xl:gap-x-64 mb-4 pr-10">
+        <div className="flex flex-col">
+          <div className="truncate font-extrabold mb-2">Discover more</div>
+          <Link to="/wishlists" className="text-sm mb-2 hover:text-gray-800">
+            Wishlists
+          </Link>
+          <Link to="/shoppinglist" className="text-sm mb-2 hover:text-gray-800">
+            Shopping list
+          </Link>
+        </div>
+        <div>
+          <div className="font-extrabold mb-2">Useful</div>
+          <div className="truncate text-sm mb-2 hover:text-gray-800">
+            Terms & Conditions
+          </div>
+          <div className="truncate text-sm mb-2 hover:text-gray-800">
+            Links to GitHub orsmth
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="text-sm">
+      © 2024 by Fredi Karu, Merilin-Grete Kakko, Voldemar Tamm
+    </div>
+  </div>
+</div>; */
+}
